68a09fa47c64cee0030657690ecdb87dcfbd9738
Abhishek Paul <abhishek.paulcp.dbs@gmail.com>
2024-08-21 23:11:15 +0530

    1. password added to user model 2. signup route added

---
+++ backend/src/routes/user.ts
@@ -0,0 +1,49 @@
+import { Hono } from "hono"
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { z } from "zod"
+import { sign } from "hono/jwt"
+
+export const userRouter = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  }
+}>()
+
+const signupInput = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  password: z.string().min(6)
+})
+
+userRouter.post("/signup", async (c) => {
+  const body = await c.req.json()
+  const { success } = signupInput.safeParse(body)
+
+  if(!success) {
+    c.status(411)
+    return c.json({ error: "Invalid inputs" })
+  }
+
+  try {
+    const prisma = new PrismaClient({ datasourceUrl: c.env.DATABASE_URL }).$extends(withAccelerate())
+    const user = await prisma.user.create({
+      data: {
+        name: body.name,
+        email: body.email,
+        password: body.password
+      }
+    })
+    const jwt = await sign({
+      id: user.id
+    }, c.env.JWT_SECRET)
+    return c.json({ data: {
+      ...user,
+      jwt: jwt
+    } })
+  } catch (error) {
+    c.status(411)
+    return c.json({ error: "Could not create user" })
+  }
+})
