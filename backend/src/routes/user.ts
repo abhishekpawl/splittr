6dafed11bc1b3f211c8fb01ddea96e0af15ea6eb
Abhishek Paul <abhishek.paulcp.dbs@gmail.com>
2024-08-22 19:11:30 +0530

    Added signin route

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -17,6 +17,11 @@ const signupInput = z.object({
   password: z.string().min(6)
 })
 
+const signinInput = z.object({
+  email: z.string().email(),
+  password: z.string().min(6)
+})
+
 userRouter.post("/signup", async (c) => {
   const body = await c.req.json()
   const { success } = signupInput.safeParse(body)
@@ -45,5 +50,39 @@ userRouter.post("/signup", async (c) => {
   } catch (error) {
     c.status(411)
     return c.json({ error: "Could not create user" })
+  }
+})
+
+userRouter.post("/signin", async (c) => {
+  const body = await c.req.json()
+  const { success } = signinInput.safeParse(body)
+
+  if(!success) {
+    c.status(403)
+    return c.json({ error: "Invalid inputs" })
+  }
+
+  try {
+    const prisma = new PrismaClient({ datasourceUrl: c.env.DATABASE_URL }).$extends(withAccelerate())
+    const user = await prisma.user.findFirst({
+      where: {
+        email: body.email,
+        password: body.password
+      }
+    })
+    if(!user) {
+      c.status(401)
+      return c.json({ error: "Invalid credentials" })
+    }
+    const jwt = await sign({
+      id: user.id
+    }, c.env.JWT_SECRET)
+    return c.json({
+      ...user,
+      jwt: jwt
+    })
+  } catch (error) {
+    c.status(411)
+    return c.json({ error: "Something went wrong" })
   }
 })
